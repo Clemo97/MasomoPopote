b44875cd14aaa88f55087ba143daa98a8a813186
atienoobwanda <atienoobwanda@gmail.com>
2022-06-20 11:08:27 +0300

    adding the login and register templates

--- tutors/views.py
+++ tutors/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def tutor(request):
     return render(request, 'tutors/tutor.html')
 
+
+def register(request):
+    return render(request, 'tutors/register.html')
