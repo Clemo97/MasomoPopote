d3119673430a7dc7fc78997a04963f7938e951ed
atienoobwanda <atienoobwanda@gmail.com>
2022-06-21 02:49:31 +0300

    creating the models

--- tutors/models.py
+++ tutors/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_tutor = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email=models.EmailField()
+
+class Tutor(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    email=models.EmailField()
+    contact = models.IntegerField(max_length=10)
+
+# class CourseCategory(models.Model):
+#     title=models.CharField(max_length=100)
+#     descriptions=models.TextField()
+
+class Course(models.Model):
+    tutor=models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    title=models.CharField(max_length=100) 
+    descriptions=models.TextField()
+    body = models.TextField()
+    course_poster = models.ImageField(upload_to='coursePoster')
+    category = models.CharField(max_length=100)
+
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True) 
+    courseInterest = models.TextField() 
+
+class test(models.Model):
+    course = models.ForeignKey(Course, on_delete = models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    
