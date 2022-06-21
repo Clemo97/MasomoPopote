d7304e3c6592fcb7c6bac3163a66ea25b6dfc542
atienoobwanda <atienoobwanda@gmail.com>
2022-06-21 08:59:04 +0300

    updating the tutor login and log out

--- tutors/urls.py
+++ tutors/urls.py
@@ -10,6 +10,6 @@ from tutors.views import TutorReg
 urlpatterns = [
     path('tut/', views.tutor, name='tutor'),
     path('register-tutor/', TutorReg.as_view(), name='tutor-reg'),
-    path('login/', auth_views.LoginView.as_view(template_name='tutors/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login-tutor/', auth_views.LoginView.as_view(template_name='tutors/login.html'), name='login'),
+    path('logout-tutor/', auth_views.LogoutView.as_view(), name='logout'),
 ]
