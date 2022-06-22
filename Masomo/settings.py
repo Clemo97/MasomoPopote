9426669546b033700832ccaf7845df52748118d9
atienoobwanda <atienoobwanda@gmail.com>
2022-06-22 05:54:50 +0300

    fixing log out redirect

--- Masomo/settings.py
+++ Masomo/settings.py
@@ -161,6 +161,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'app/static'),
 )
 
+LOGOUT_REDIRECT_URL = 'home'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
