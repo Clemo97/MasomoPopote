4ade17d83dbfb358ad0c7010ab9e28f52f8e1fce
atienoobwanda <atienoobwanda@gmail.com>
2022-06-22 03:42:06 +0300

    adding postgres database configurations

--- Masomo/settings.py
+++ Masomo/settings.py
@@ -95,8 +95,14 @@ WSGI_APPLICATION = 'Masomo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'masomo',
+        'USER': 'atieno',
+        'PASSWORD': 'mishi',
+        'HOST':'localhost',
+        'PORT':''
     }
 }
 
