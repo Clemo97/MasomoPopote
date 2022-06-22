2947c86a82cfc7bffd83ee4e9970f09d939fd3c6
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-06-22 14:39:50 +0300

    added an image in login

--- Masomo/settings.py
+++ Masomo/settings.py
@@ -97,10 +97,18 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'masomo',
+        # 'USER': 'atieno',
+        # 'PASSWORD': 'mishi',
+        # 'HOST':'localhost',
+        # 'PORT':''
+        
+        
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'masomo',
-        'USER': 'atieno',
-        'PASSWORD': 'mishi',
+        'NAME': 'students',
+        'USER': 'moringa',
+        'PASSWORD': 'Access',
         'HOST':'localhost',
         'PORT':''
     }
--- students/urls.py
+++ students/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 from . import views
 from students.views import studentReg
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path ('dashboard/', views.students, name='students'),
@@ -18,3 +18,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
