c843229d99ccae0e90737ede9a02124517d62197
Moses Karani <karanim594@gmail.com>
2022-06-22 16:05:53 +0300

    optimized login

--- Masomo/settings.py
+++ Masomo/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'masomo',
-        'USER': 'atieno',
-        'PASSWORD': 'mishi',
+        'USER': 'moringa',
+        'PASSWORD': 'morces',
         'HOST':'localhost',
         'PORT':''
     }
