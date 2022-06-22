9cb281859743be65199820150578e060e9aa24db
atienoobwanda <atienoobwanda@gmail.com>
2022-06-22 03:01:19 +0300

    updating models

--- tutors/models.py
+++ tutors/models.py
@@ -9,13 +9,11 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=100)
     email=models.EmailField()
 
+
 class Tutor(models.Model):
     user = models.OneToOneField(User, related_name='tutor', on_delete = models.CASCADE, primary_key = True)
-    # contact = models.IntegerField()
-
-# class CourseCategory(models.Model):
-#     title=models.CharField(max_length=100)
-#     descriptions=models.TextField()
+    def __str__(self):
+            return self.user.first_name
 
 class Course(models.Model):
     tutor=models.ForeignKey(Tutor, on_delete=models.CASCADE)
@@ -24,15 +22,23 @@ class Course(models.Model):
     body = models.TextField()
     course_poster = models.ImageField(upload_to='coursePoster')
     category = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
 
 
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True) 
-    courseInterest = models.TextField() 
+    courseInterest = models.TextField()
+
+    def __str__(self):
+        return self.user.first_name
 
 class test(models.Model):
     course = models.ForeignKey(Course, on_delete = models.CASCADE)
     title = models.CharField(max_length=100)
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
 
